c78252bbdaee04474765f2c6b54756ca52671ac8
LeonardoValdez23 <160821059+LeonardoValdez23@users.noreply.github.com>
2025-06-02 11:28:28 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://44.202.71.251/api/devices';
+const API_URL = 'http://18.207.223.229/api/devices';
 const tablaBody = document.getElementById('tablaDatos');
 const statusPrincipal = document.getElementById('statusPrincipal');
 
